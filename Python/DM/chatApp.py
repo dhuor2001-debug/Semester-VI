d0d2688a51cdc368e5817b2c593a4f8480a4562b
dhuor2001-debug <dhuor2001@gmail.com>
2026-03-28 13:59:50 +0530

    Send button function created. it is the send line after imports.

--- Python/DM/chatApp.py
+++ Python/DM/chatApp.py
@@ -1,6 +1,17 @@
 from tkinter import *
 from tkinter import scrolledtext
 
+#SendButton Function
+def SendFunc():
+    UserInput = InputText.get("1.0",END).strip()
+    DisplayText.config(state=NORMAL)
+    DisplayText.insert(END,"You: "+ UserInput + "\n")
+    DisplayText.insert(END,"Bot: "+ "Hi, Welcome to the Chat App!" + "\n")
+    DisplayText.config(state=DISABLED)
+    InputText.delete("1.0", END)
+
+
+
 root = Tk()
 root.title("Chat APP")
 
@@ -13,7 +24,7 @@ DisplayText.pack(fill=BOTH,expand=TRUE)
 InputText= scrolledtext.ScrolledText(root, height=5, wrap = WORD)
 InputText.pack(fill=X, padx=5, pady = 5)
 
-SendButton = Button(root, text="Send", font=("italy",12), bg="lightgreen", fg="red")
+SendButton = Button(root, text="Send", font=("italy",12,"bold"),command=SendFunc, bg="lightgreen", fg="red")
 SendButton.pack(fill=X, padx=5,pady=5)
 
 root.mainloop()
